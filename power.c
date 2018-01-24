868c4f49b95e2d9c79892ca3d941a82af7dca6fe
kanigayu <31438605+kanigayu@users.noreply.github.com>
2018-01-24 16:37:34 +0530

    power.c
    power

---
+++ power.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int base,power;
+int answer=1,i=0;
+printf("Enter the base")
+scanf("%d",&base);
+printf("Enter the power");
+scanf("%d",&power);
+{
+for(i = 0;i<power;i++)
+{
+        answer= answer * base;
+    }
+    printf("power is%d",answer);
+    }
+    retuen 0;
+    }
+    
